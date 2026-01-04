227da4f19a8d8b95f726311b8baebcc337737a18
Gwynn <haytamhsa799@gmail.com>
2026-01-04 15:48:56 -0800

    Delete no_need_to_this directory

--- no_need_to_this/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from stars import router as stars_data
-from planets import router as planets_data
-import requests,json
-
-app = FastAPI()
-
-app.include_router(stars_data)
-app.include_router(planets_data)
